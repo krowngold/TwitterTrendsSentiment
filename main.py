6165d306ca0c20518ca40434aa8e08460623edb8
krowngold <Noah.Krohngold@gmail.com>
2019-07-26 17:18:48 -0700

    Uploading initial files necessary to begin work (including .yaml, .py, and html files)

---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+import logging
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/main.html')
+        self.response.write(template.render(template_vars))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
