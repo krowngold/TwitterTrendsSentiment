ff138936a306b0a1611a992650d95b96b508e083
ethan-buranday <buranday.ethan@gmail.com>
2019-07-30 11:06:04 -0700

    Added JS folder to app.yaml

---
+++ js/animation.js
@@ -0,0 +1,30 @@
+// const headerElement =
+//   document.querySelector(`#header`);
+//
+// // setInterval (() => {
+// //   headerElement.style.color = `red`;
+// // }, 1000);
+
+// const subHeaderElement =
+//   document.querySelector(`#subheader span`);
+//
+//   subHeaderElement.addEventListener('mouseover', () => {
+//     subHeaderElement.style.color = 'blue';
+//   });
+//
+//   subHeaderElement.addEventListener('mouseleave', () => {
+//     subHeaderElement.style.color = 'black';
+//   });
+//
+// subHeaderElement.addEventListener('mouseover', () => {
+//   subHeaderElement.innerHTML = 'Hello!';
+// });
+//
+// subHeaderElement.addEventListener('mouseleave', () => {
+//   subHeaderElement.innerHTML = 'Hello!';
+// });
+
+
+// subHeaderElement.addEventListener('mouseover', () => {
+//   subHeaderElement.addClassList.add(`blueText`);
+// });
