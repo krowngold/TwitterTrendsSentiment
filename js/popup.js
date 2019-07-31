fb540591f12259f7a524ad34ee05a38c71e5d62f
ethan-buranday <buranday.ethan@gmail.com>
2019-07-31 11:32:35 -0700

    Renamed JS file

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+let location = {{user_search}};
+
+function alertSmallTown () {
+  if alertSmallTown (user_search = user_search) {
+    alert("So there's a city called {{user_search}}? Here's United States though....")
+  }
+
+};
